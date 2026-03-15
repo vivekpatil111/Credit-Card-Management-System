a959aa12c913977cee33dffe5c6844e536a89b79
Vivek N Patil <143700819+vivekpatil111@users.noreply.github.com>
2026-03-16 03:01:59 +0530

    initial commit

--- ccms/app.py
+++ ccms/app.py
@@ -877,4 +877,4 @@ if __name__ == '__main__':
         import init_db
         init_db.build_database()
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
